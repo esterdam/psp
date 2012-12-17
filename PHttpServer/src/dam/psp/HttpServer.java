2a5f5f4ce13cdddf638978d1bd40e4fd90db8a10
esterdam <esther@pcI5-02.(none)>
2012-12-17 09:52:17 +0100

    Actualizando PHttpServer

--- PHttpServer/src/dam/psp/HttpServer.java
+++ PHttpServer/src/dam/psp/HttpServer.java
@@ -1,6 +1,10 @@
 package dam.psp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,13 +14,19 @@ public class HttpServer {
 
 	public static void main (String[]args) throws IOException {
 		
-		String newLine = "\r\n";
-		int puerto = 8080;
+		final String newLine = "\r\n";
+		final int puerto = 8080;
+		final String fileNameError404 = "fileError404.html";
+		final String response200 = "HTTP/1.0 200 OK";
+		final String response404 = "HTTP/1.0 404 Not Found";
 		ServerSocket serverSocket = new ServerSocket(puerto);
 		
 		Socket socket = serverSocket.accept();
 		
 		Scanner scanner = new Scanner(socket.getInputStream());
+		
+		String fileName = "index.html";
+		String response = response200;
 		
 		while(true) {
 			String line = scanner.nextLine();
@@ -26,11 +36,28 @@ public class HttpServer {
 			}
 		}
 		
-		PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-		printWriter.print("HTTP /1.0 404 Not Found" + newLine);
-		printWriter.print(newLine);
+		File file = new File(fileName);
+		if(!file.exists()) {
+			fileName = fileNameError404;
+			response = response404;
+		}
 		
-		printWriter.flush();
+		FileInputStream fileInputStream = new FileInputStream(fileName);
+		
+		String header = response + newLine + newLine;
+		byte[]headerBuffer = header.getBytes();
+		
+		OutputStream outputStream = socket.getOutputStream();
+		outputStream.write(headerBuffer, 0, headerBuffer.length);
+		
+		final int bufferSize = 2048;
+		byte[]buffer = new byte[bufferSize];
+		int count = 0;
+		
+		while( (count = fileInputStream.read(buffer)) != -1 )
+			outputStream.write(buffer, 0, count);
+		
+		fileInputStream.close();
 		
 		socket.close();
 		serverSocket.close();
